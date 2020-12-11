329a7141aab6f324bef5dcb2b95c101b157a819c
Dyorgio Nascimento <dyorgio@gmail.com>
2020-12-11 14:15:18 -0300

    Include forced option and reduce syscalls
    Included -f (forced mode) to only return 1 (not a clone) if cannot find or read files.
    Replaced lseek and fcntl(F_LOG2PHYS) by only one call to fcntl(F_LOG2PHYS_EXT).
    Return 1 (not a clone) if both files are the same.

--- clone_checker.c
+++ clone_checker.c
@@ -6,6 +6,7 @@
 //
 //  Created by Dyorgio Nascimento on 2020-12-10.
 //
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -17,43 +18,67 @@
 
 // declare methods
 int compare_blocks(int block_size, char *filenameA, char *filenameB, int fdA, int fdB);
-void check_disk_fs(char *filename);
-struct stat check_file(char *filename);
+void check_disk_fs(char *filename, bool is_forced_mode);
+struct stat check_file(char *filename, bool is_forced_mode);
 
 // entrypoint
 int main(int args_count, char **args) {
 
-  if (args_count < 3) {
-    fprintf(stderr, "Usage: clone_checker fileA fileB\n");
+  bool is_forced_mode = false;
+  int opt;
+  while ( (opt = getopt(args_count, args, "f")) != -1) {
+       switch (opt) {
+       case 'f': is_forced_mode = true; break;
+       default:
+           fprintf(stderr, "Usage: %s [-f] fileA fileB\n", args[0]);
+           exit(EXIT_FAILURE);
+       }
+   }
+  if (args_count - optind < 2) {
+    fprintf(stderr, "Usage: %s [-f] fileA fileB\n", args[0]);
     exit(EXIT_FAILURE);
   }
 
-  check_disk_fs(args[1]);
-  check_disk_fs(args[2]);
+  char* filenameA = args[optind];
+  char* filenameB = args[optind + 1];
 
-  struct stat statA = check_file(args[1]);
-  struct stat statB = check_file(args[2]);
+  check_disk_fs(filenameA, is_forced_mode);
+  check_disk_fs(filenameB, is_forced_mode);
 
-  if (statA.st_dev != statB.st_dev || statA.st_size != statB.st_size || statA.st_blocks != statB.st_blocks) {
-    // clones only are supported on same device and have same size em blocks count
+  struct stat statA = check_file(filenameA, is_forced_mode);
+  struct stat statB = check_file(filenameA, is_forced_mode);
+
+  if (statA.st_dev != statB.st_dev || statA.st_size != statB.st_size
+      || statA.st_blocks != statB.st_blocks || statA.st_ino == statB.st_ino) {
+    // clones only are supported on same device and have same size em blocks count, a file cannot be a clone of itself
     fprintf(stdout,"1\n");
     exit(EXIT_SUCCESS);
   }
 
-  int fdA = open(args[1], O_RDONLY);
+  int fdA = open(filenameA, O_RDONLY);
   if (fdA < 0 ) {
-    fprintf(stderr,"%s: Cannot open. %s\n", args[1], strerror(errno));
-    exit(EXIT_FAILURE);
+    fprintf(stderr,"%s: Cannot open. %s\n", filenameA, strerror(errno));
+    if (is_forced_mode) {
+      fprintf(stdout,"1\n");
+      exit(EXIT_SUCCESS);
+    } else {
+      exit(EXIT_FAILURE);
+    }
   }
 
-  int fdB = open(args[2], O_RDONLY);
+  int fdB = open(filenameB, O_RDONLY);
   if ( fdB < 0 ) {
-    fprintf(stderr,"%s: Cannot open. %s\n", args[2], strerror(errno));
+    fprintf(stderr,"%s: Cannot open. %s\n", filenameB, strerror(errno));
     close(fdA);
-    exit(EXIT_FAILURE);
+    if (is_forced_mode) {
+      fprintf(stdout,"1\n");
+      exit(EXIT_SUCCESS);
+    } else {
+      exit(EXIT_FAILURE);
+    }
   }
 
-  int result = compare_blocks(statA.st_blksize, args[1], args[2], fdA, fdB);
+  int result = compare_blocks(statA.st_blksize, filenameA, filenameB, fdA, fdB);
 
   close(fdA);
   close(fdB);
@@ -62,7 +87,12 @@ int main(int args_count, char **args) {
     fprintf(stdout,"%i\n", result);
     exit(EXIT_SUCCESS);
   } else {
-    exit(EXIT_FAILURE);
+    if (is_forced_mode) {
+      fprintf(stdout,"1\n");
+      exit(EXIT_SUCCESS);
+    } else {
+      exit(EXIT_FAILURE);
+    }
   }
 }
 
@@ -73,40 +103,32 @@ int compare_blocks(int block_size, char *filenameA, char *filenameB, int fdA, in
   struct log2phys physB;
 
   for ( off_t offset = 0; sts >= 0; offset += block_size ) {
-    // Seek files to current position
-    sts = lseek(fdA, offset, SEEK_SET);
-    if ( sts < 0 ) {
-      fprintf(stderr,"%s: Cannot seek. %ld %s\n", filenameA, sts, strerror(errno));
-      return -1;
-    }
-    sts = lseek(fdB, offset, SEEK_SET);
-    if ( sts < 0 ) {
-      fprintf(stderr,"%s: Cannot seek. %ld %s\n", filenameB, sts, strerror(errno));
-      return -1;
-    }
-
+    physA.l2p_devoffset = offset;
     // get current blocks physical location
-    sts = fcntl(fdA, F_LOG2PHYS, &physA);
+    sts = fcntl(fdA, F_LOG2PHYS_EXT, &physA);
     if ( sts < 0 && errno == ERANGE ) {
-      sts = fcntl(fdB, F_LOG2PHYS, &physB);
+      physB.l2p_devoffset = offset;
+      sts = fcntl(fdB, F_LOG2PHYS_EXT, &physB);
       if ( sts < 0 && errno == ERANGE ) {
         // both files seeked to the end with same offsets
         return 0;
       } else if ( sts < 0 ) {
-        fprintf(stderr,"%s: Cannot LOG2PHYS. %ld %s\n", filenameB, sts, strerror(errno));
+        fprintf(stderr,"%s: Cannot convert logical to physical offset. %i %s\n", filenameB, errno, strerror(errno));
         return -1;
       }
       break;
     } else if ( sts < 0 ) {
-      fprintf(stderr,"%s: Cannot LOG2PHYS. %ld %s\n", filenameA, sts, strerror(errno));
+      fprintf(stderr,"%s: Cannot convert logical to physical offset. %i %s\n", filenameA, errno, strerror(errno));
       return -1;
     }
 
-    sts = fcntl(fdB, F_LOG2PHYS, &physB);
+    physB.l2p_devoffset = offset;
+    sts = fcntl(fdB, F_LOG2PHYS_EXT, &physB);
     if ( sts < 0 && errno == ERANGE ) {
       // insanity check, size of files already verified before
       break;
     } else if ( sts < 0 ) {
+      fprintf(stderr,"%s: Cannot convert logical to physical offset. %i %s\n", filenameB, errno, strerror(errno));
       return -1;
     }
 
@@ -120,26 +142,41 @@ int compare_blocks(int block_size, char *filenameA, char *filenameB, int fdA, in
   return 1;
 }
 
-void check_disk_fs(char *filename) {
+void check_disk_fs(char *filename, bool is_forced_mode) {
   struct statfs fs;
   if( statfs(filename, &fs) == 0 ) {
     if( strcmp(fs.f_fstypename, "apfs") != 0) {
       fprintf(stderr, "%s: Only APFS is supported: %s\n", filename, fs.f_fstypename);
-      exit(EXIT_FAILURE);
+      if (is_forced_mode) {
+        fprintf(stdout,"1\n");
+        exit(EXIT_SUCCESS);
+      } else {
+        exit(EXIT_FAILURE);
+      }
     }
   }
 }
 
-struct stat check_file(char *filename) {
+struct stat check_file(char *filename, bool is_forced_mode) {
   struct stat st;
-  if ( stat(filename, &st) < 0) {
+  if ( stat(filename, &st) < 0 ) {
     fprintf(stderr, "%s: No such file\n", filename);
-    exit(EXIT_FAILURE);
+    if (is_forced_mode) {
+      fprintf(stdout,"1\n");
+      exit(EXIT_SUCCESS);
+    } else {
+      exit(EXIT_FAILURE);
+    }
   }
 
   if ( (st.st_mode & S_IFMT) != S_IFREG ) {
     fprintf(stderr, "%s: Not a regular file\n", filename);
-    exit(EXIT_FAILURE);
+    if (is_forced_mode) {
+      fprintf(stdout,"1\n");
+      exit(EXIT_SUCCESS);
+    } else {
+      exit(EXIT_FAILURE);
+    }
   }
   return st;
 }
